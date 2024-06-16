d615c869583250415e4db3408086f65c74167c55
Olga Semenova <helen597597@gmail.com>
2024-06-16 16:53:07 +0500

    add models, migrations

--- modules/models.py
+++ modules/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from config.settings import AUTH_USER_MODEL
+NULLABLE = {'null': True, 'blank': True}
+
 
 # Create your models here.
+class Module(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+    description = models.CharField(max_length=300, verbose_name='Описание', **NULLABLE)
+    owner = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Владелец')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Образовательный модуль'
+        verbose_name_plural = 'Образовательные модули'
+        ordering = ('title',)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+NULLABLE = {"null": True, "blank": True}
+
+
 # Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="почта")
+    phone = models.CharField(max_length=35, verbose_name="телефон", **NULLABLE)
+    avatar = models.ImageField(upload_to="users/",
+                               verbose_name="аватар", **NULLABLE)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.email}"
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
+        ordering = ("email",)
